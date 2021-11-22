ee5036548b1b6cbe8b016e0b17e9eb384bc49372
Azra Eiden <carter.eiden@gmail.com>
2021-11-22 02:01:24 -0500

    Lambda Extensions Aggregated

---
+++ function/lambda_function.py
@@ -0,0 +1,289 @@
+import boto3
+import botocore
+from botocore.exceptions import ClientError
+# import jsonschema
+import json
+import traceback
+
+from extutil import remove_none_attributes, gen_log, creturn, handle_common_errors, \
+    account_context, component_safe_name, ExtensionHandler, ext
+
+eh = ExtensionHandler()
+ALLOWED_RUNTIMES = ["python3.9", "python3.8", "python3.6", "python3.7", "nodejs14.x", "nodejs12.x", "nodejs10.x", "ruby2.7", "ruby2.5"]
+
+def lambda_handler(event, context):
+    try:
+        print(event)
+        eh.capture_event(event)
+
+        region = account_context(context)['region']
+        prev_state = event.get("prev_state")
+        op = event.get("op")
+
+        cdef = event.get("component_def")
+        cname = event.get("component_name")
+
+        bucket = event.get("bucket")
+        project_code = event.get("project_code")
+        repo_id = event.get("repo_id")
+        object_name = event.get("s3_object_name")
+        runtime = cdef.get("runtime") or "python3.8"
+        if runtime not in ALLOWED_RUNTIMES:
+            return creturn(200, 0, error=f"runtime {runtime} not allowed, please choose one of {ALLOWED_RUNTIMES}")
+
+        handler = cdef.get("handler") or get_default_handler(runtime)
+        description = cdef.get("description") or f"Lambda for component {cname}"
+        timeout = cdef.get("timeout") or 5
+        memory_size = cdef.get("memory_size") or 256
+        environment = {"Variables": {k: str(v) for k,v in cdef.get("environment_variables").items()}} if cdef.get("environment_variables") else None
+
+        tags = cdef.get("tags")
+        role = cdef.get("role", {})
+        # if role and (not "lambda" in role.get("role_services", [])) and (not "lambda.amazonaws.com" in role.get("role_services", [])):
+        #     return creturn(200, 0, error=f"The referenced role must have lambda in its list of trusted services")
+
+        role_arn = cdef.get("role_arn") or role.get("arn")
+        if not role_arn:
+            return creturn(200, 0, error=f"Must provide a role_arn. Please use either the role or role_arn keywords")
+
+        layer_arns = cdef.get("layer_arns") or []
+        layers = cdef.get("layers") or []
+        if layers:
+            try:
+                layer_arns.extend(list(map(lambda x: x['version_arn'], layers)))
+            except:
+                eh.add_log("Invalid Layer Parameters", {"layers": layers})
+                eh.perm_error("Invalid layer parameters", 0)
+
+        function_name = cdef.get("name") or component_safe_name(project_code, repo_id, cname)
+        pass_back_data = event.get("pass_back_data", {})
+
+        desired_config = remove_none_attributes({
+            "FunctionName": function_name,
+            "Description": description,
+            "Handler": handler,
+            "Role": role_arn,
+            "Timeout": timeout,
+            "MemorySize": memory_size,
+            "Environment": environment,
+            "Runtime": runtime,
+            "Layers": layer_arns or None
+        })
+
+        if pass_back_data:
+            pass
+        elif op == "upsert":
+            eh.add_op("get_lambda")
+            eh.add_op("gen_props")
+        elif op == "delete":
+            eh.add_op("remove_old", function_name)
+
+        get_function(prev_state, function_name, desired_config)
+        create_function(function_name, desired_config, bucket, object_name)
+        update_function_configuration(function_name, desired_config)
+        update_function_code(function_name, bucket, object_name)
+        remove_function()
+        gen_props(function_name, region)
+        return eh.finish()
+
+    except Exception as e:
+        msg = traceback.format_exc()
+        print(msg)
+        eh.add_log("Uncovered Error", {"error": str(e)}, is_error=True)
+        eh.declare_return(200, 0, error_code=str(e))
+        return eh.finish()
+
+def get_default_handler(runtime):
+    if runtime.startswith("python") or runtime.startswith("ruby"):
+        return "lambda_function.lambda_handler"
+    elif runtime.startswith("node"):
+        return "index.handler"
+
+@ext(handler=eh, op="get_lambda")
+def get_function(prev_state, function_name, desired_config):
+    lambda_client = boto3.client("lambda")
+
+    if prev_state and prev_state.get("props", {}).get("name"):
+        old_function_name = prev_state["props"]["name"]
+        if old_function_name and (old_function_name != function_name):
+            eh.add_op("remove_old", {"name": old_function_name, "create_and_remove": True})
+            eh.add_op("create_function", function_name)
+            return 0
+
+    try:
+        function = lambda_client.get_function(
+            FunctionName=function_name
+        )
+
+        current_config = function['Configuration']
+        print(f'function config = {current_config}')
+
+        function_arn = function.get("FunctionArn")
+        eh.add_props({"arn": function_arn})
+        eh.add_op("update_function_code")
+        eh.add_log("Got Existing Lambda Function", function)
+        for k, v in desired_config.items():
+            if k == "Layers":
+                continue
+            elif v != current_config.get(k):
+                eh.add_log("Desired Config Doesn't Match", {"current": current_config, "desired": desired_config})
+                eh.add_op("update_function_configuration")
+
+        if not eh.ops.get("update_function_configuration"):
+            current_layer_arns = set(map(lambda x: x['Arn'], current_config.get("Layers") or []))
+            desired_layer_arns = set(desired_config.get("Layers") or [])
+            if current_layer_arns != desired_layer_arns:
+                eh.add_log("Desired Layers Don't Match", {"current": current_layer_arns, "desired": desired_layer_arns})
+                eh.add_op("update_function_configuration")
+                    
+        #Store hash?
+        # update_function_code = True
+
+    except ClientError as e:
+        if e.response['Error']['Code'] in ['ResourceNotFound', 'ResourceNotFoundException']:
+            eh.add_op("create_function")
+            eh.add_log("Function Does Not Exist", {"name": function_name})
+        else:
+            handle_common_errors(e, eh, "Get Function Failed", 0)
+    
+
+@ext(handler=eh, op="create_function")
+def create_function(function_name, desired_config, bucket, object_name):
+    lambda_client = boto3.client("lambda")
+
+    def retry_create(message):
+        eh.add_log("Potential Race Condition Hit", {"Error": message}, is_error=True)
+        eh.retry_error(message, 20)
+
+    try:
+        create_params = desired_config
+        create_params["Code"] = {
+                "S3Bucket": bucket,
+                "S3Key": object_name
+            }
+
+        lambda_response = lambda_client.create_function(**remove_none_attributes(create_params))
+
+        # function_arn = lambda_response.get("FunctionArn")
+        eh.add_log("Created Lambda Function", lambda_response)
+
+    except ClientError as e:
+        print(str(e))
+        if e.response['Error']['Code'] in ['ResourceConflictException', 'ResourceConflict']:
+            eh.add_log("Lambda Already Exists", {"function_name": function_name})
+            eh.add_op("update_function_code")
+            eh.add_op("update_function_configuration")
+        elif str(e) == "An error occurred (InvalidParameterValueException) when calling the CreateFunction operation: The role defined for the function cannot be assumed by Lambda.":
+            #Need to check whether this is actually a race condition or whether it is a role_policy issue
+            retry_create(str(e))
+        elif str(e).startswith("An error occurred (InvalidParameterValueException) when calling the CreateFunction operation: Lambda was unable to configure access to your environment variables because the KMS key is invalid for CreateGrant. Please check your KMS key settings. KMS Exception: InvalidArnException KMS Message: ARN does not refer to a valid principal:"):
+            #Need to check whether this is actually a race condition or whether it is a role_policy issue
+            retry_create(str(e))
+        else:
+            handle_common_errors(e, eh, "Create Function Failed:", 20, [
+                'InvalidParameterValue', 'CodeStorageExceeded', 'CodeVerificationFailed', 
+                'InvalidCodeSignature', 'CodeSigningConfigNotFound'
+            ])
+
+
+@ext(handler=eh, op="update_function_configuration")
+def update_function_configuration(function_name, desired_config):
+    lambda_client = boto3.client("lambda")
+    print(f"Inside update config, desired_config = {desired_config}")
+    try:
+        _ = desired_config.pop("Code")
+    except:
+        pass
+    try:
+        lambda_response = lambda_client.update_function_configuration(
+            **desired_config
+        )
+        eh.add_log("Updated Function Config", lambda_response)
+
+    except ClientError as e:
+        handle_common_errors(e, eh, "Config Update Failed:", 60, [
+            'PreconditionFailed', 'CodeVerificationFailed', 
+            'InvalidCodeSignature', 'CodeSigningConfigNotFound'
+            ])
+
+
+@ext(handler=eh, op="update_function_code")
+def update_function_code(function_name, bucket, object_name):
+    lambda_client = boto3.client("lambda")
+
+    try:
+        lambda_response = lambda_client.update_function_code(
+            FunctionName=function_name,
+            S3Bucket=bucket,
+            S3Key=object_name
+        )
+        # function_arn = lambda_response.get("FunctionArn")
+        eh.add_log("Updated Function Code", lambda_response)
+
+    except ClientError as e:
+        handle_common_errors(e, eh, "Code Update Failed:", 75, [
+            'InvalidParameterValue', 'CodeStorageExceeded', 
+            'PreconditionFailed', 'CodeVerificationFailed', 
+            'InvalidCodeSignature', 'CodeSigningConfigNotFound'
+            ])
+        
+
+@ext(handler=eh, op="gen_props")
+def gen_props(function_name, region):
+    lambda_client = boto3.client("lambda")
+
+    try:
+        response = lambda_client.get_function(
+            FunctionName=function_name
+        )
+        function = response["Configuration"]
+        eh.add_props({
+            "name": function['FunctionName'],
+            "arn": function['FunctionArn'],
+            "code_size": function['CodeSize'],
+            "code_sha": function['CodeSha256'],
+            "last_modified": function["LastModified"],
+            "master_arn": function.get("MasterArn"),
+            "layers": list(map(lambda x:x["Arn"], function.get("Layers", [])))
+        })
+        eh.add_links({
+            "Function": gen_lambda_link(function_name, region)
+        })
+    except ClientError as e:
+        handle_common_errors(e, eh, "Get Props Failed: ", 96)
+
+
+def gen_iam_policy_arn(policy_name, account_number, path="/"):
+    #arn:aws:iam::227993477930:policy/3aba481ac88bcbc5d94567e9f93339a7-iam
+    return f"arn:aws:iam::{account_number}:policy{path}{policy_name}"
+
+#This may be different with "paths"
+def gen_lambda_arn(function_name, region, account_number):
+    #arn:aws:iam::227993477930:policy/3aba481ac88bcbc5d94567e9f93339a7-iam
+    return f"arn:aws:iam:{region}:{account_number}:function:{function_name}"
+
+#I bet this is different with "paths"
+def gen_lambda_link(function_name, region):
+    return f"https://console.aws.amazon.com/lambda/home?region={region}#/functions/{function_name}"
+
+@ext(handler=eh, op="remove_old")
+def remove_function():
+    lambda_client = boto3.client("lambda")
+
+    op_def = eh.ops['remove_old']
+    function_to_delete = op_def['function_name']
+    create_and_delete = op_def.get("create_and_delete") or False
+
+    try:
+        delete_response = lambda_client.delete_function(
+            FunctionName=function_to_delete
+        )
+        eh.add_log(f"Deleted Function", delete_response)
+
+    except ClientError as e:
+        if e.response['Error']['Code'] == 'NoSuchEntityException':
+            eh.add_log(f"Function Does Not Exist", {"function_name": function_to_delete})
+        else:
+            eh.retry_error(str(e), 90 if create_and_delete else 15)
+            eh.add_log(f"Error Deleting Function", {"name": function_to_delete}, True)
+
---
+++ layer/lambda_function.py
@@ -0,0 +1,208 @@
+import boto3
+import botocore
+# import jsonschema
+import json
+import traceback
+
+# {
+#     "prev_state": prev_state,
+#     "component_def": component_def,
+#     "component_name": component_name,
+#     "op": op,
+#     "s3_object_name": object_name,
+#     "pass_back_data": pass_back_data,
+#     "bucket": bucket,
+#     "repo_id": repo_id,
+#     "project_code": project_code
+# }
+
+ALLOWED_RUNTIMES = ["python3.9", "python3.8", "python3.6", "python3.7", "nodejs14.x", "nodejs12.x", "nodejs10.x", "ruby2.7", "ruby2.5"]
+
+
+from extutil import remove_none_attributes, account_context, ExtensionHandler, ext, \
+    current_epoch_time_usec_num, component_safe_name
+
+eh = ExtensionHandler()
+
+def lambda_handler(event, context):
+    try:
+        print(f"event = {event}")
+        # account_number = account_context(context)['number']
+        region = account_context(context)['region']
+        eh.capture_event(event)
+        prev_state = event.get("prev_state")
+        cdef = event.get("component_def")
+        project_code = event.get("project_code")
+        repo_id = event.get("repo_id")
+        cname = event.get("component_name")
+        layer_name = cdef.get("name") or component_safe_name(project_code, repo_id, cname)
+        bucket = event.get("bucket")
+        object_name = event.get("s3_object_name")
+
+        pass_back_data = event.get("pass_back_data", {})
+        if pass_back_data:
+            pass
+        elif event.get("op") == "upsert":
+            if prev_state and prev_state.get("props") and (prev_state.get("props").get("name")) != layer_name:
+                eh.add_op("remove_layer_versions", {"name":prev_state.get("props").get("name")})
+                eh.add_op("publish_layer_version")
+            elif prev_state:
+                eh.add_op("check_if_update_required")
+            else:
+                eh.add_op("publish_layer_version")
+
+        elif event.get("op") == "delete":
+            eh.add_op("remove_layer_versions", layer_name)
+
+        check_if_update_required(prev_state, bucket, object_name)
+        publish_layer_version(layer_name, cdef, bucket, object_name, region)
+        remove_layer_versions(event.get("op"))
+
+        return eh.finish()
+
+    except Exception as e:
+        msg = traceback.format_exc()
+        print(msg)
+        eh.add_log("Uncovered Error", {"error": msg}, is_error=True)
+        eh.declare_return(200, 0, error_code=str(e))
+        return eh.finish()
+
+def get_s3_etag(bucket, object_name):
+    s3 = boto3.client("s3")
+
+    try:
+        s3_metadata = s3.head_object(Bucket=bucket, Key=object_name)
+        print(f"s3_metadata = {s3_metadata}")
+        eh.add_state({"zip_etag": s3_metadata['ETag']})
+    except s3.exceptions.NoSuchKey:
+        eh.add_log("Cound Not Find Zip", {"bucket": bucket, "key": object_name})
+        eh.retry_error("Object Not Found")
+
+@ext(handler=eh, op="check_if_update_required")
+def check_if_update_required(prev_state, bucket, object_name):
+    lambda_client = boto3.client("lambda")
+
+    props = prev_state.get("props") or {}
+    if not props.get("version_arn"):
+        eh.add_op("publish_layer_version")
+
+    else:
+        try:
+            layer_response = lambda_client.get_layer_version_by_arn(props['version_arn'])
+            eh.add_log("Got Existing Layer Version", layer_response)
+            code_etag = layer_response.get("description")
+            print(f"code_etag = {code_etag}")
+        except botocore.exceptions.ClientError as e:
+            print(str(e))
+            if e.response['Error']['Code'] in ["ResourceNotFoundException", "ResourceNotFound"]:
+                eh.add_log("Expected Layer Version Does Not Exist", {'layer_name': props.get("name")})
+                eh.add_op("publish_layer_version")
+                return None
+            else:
+                eh.add_log("Error in Getting Layer Version", {'layer_name': props.get("name")})
+                eh.retry_error(e.response['Error']['Code'])
+                return None
+    
+        get_s3_etag(bucket, object_name)
+        if eh.state.get('zip_etag'):
+            if code_etag == eh.state['zip_etag']:
+                eh.add_log("No Update Needed", {"etag": eh.state.get('zip_etag')})
+                eh.add_props(prev_state.get("props"))
+                eh.add_links(prev_state.get("links"))
+                eh.add_state(prev_state.get("state"))
+            else:
+                eh.add_log("New Layer Version Required", {"etag": eh.state.get('zip_etag'), "code_etag": code_etag})
+                eh.add_op("publish_layer_version")
+        else:
+            return None
+
+
+@ext(handler=eh, op="publish_layer_version")
+def publish_layer_version(layer_name, cdef, bucket, object_name, region):
+    lambda_client = boto3.client("lambda")
+    if not eh.state.get("zip_etag"):
+        get_s3_etag(bucket, object_name)
+    
+    description = eh.state.get("zip_etag")
+    compatible_runtimes = cdef.get("compatible_runtimes") or ["python3.9", "python3.8", "python3.6", "python3.7"]    
+    if not isinstance(compatible_runtimes, list):
+        eh.perm_error("compatible_runtimes must be a list of strings")
+    
+    desired_config = remove_none_attributes({
+        "LayerName": layer_name,
+        "Description": description,
+        "Content": {
+            "S3Bucket": bucket,
+            "S3Key": object_name
+        },
+        "CompatibleRuntimes": compatible_runtimes,
+    })
+    print(f"Inside publish_layer_version, desired_config = {desired_config}")
+
+    try:
+        lambda_response = lambda_client.publish_layer_version(
+            **desired_config
+        )
+        eh.add_log("Published Layer Version", lambda_response)
+        eh.add_props({
+            "arn": lambda_response['LayerArn'],
+            "version_arn": lambda_response['LayerVersionArn'],
+            "version": lambda_response['Version']
+        })
+        eh.add_links({"Layer": gen_layer_link(layer_name, region)})
+        if lambda_response['Version'] != 1:
+            eh.add_op("remove_layer_versions", {"name": layer_name, "version": lambda_response['Version']})
+
+    except botocore.exceptions.ClientError as e:
+        print(str(e))
+        if e.response['Error']['Code'] in ["InvalidParameterValueException", "CodeStorageExceededException"]:
+            eh.add_log("Failure to Publish Layer Version", {'config': desired_config})
+            eh.perm_error(str(e))
+        else:
+            eh.add_log("Error in Getting Layer Version", {'layer_name': props.get("name")})
+            eh.retry_error(e.response['Error']['Code'])
+
+
+@ext(handler=eh, op="remove_layer_versions")
+def remove_layer_versions(op):
+    lambda_client = boto3.client("lambda")
+
+    layer_name = eh.ops['remove_layer_versions']['name']
+    safe_version = eh.ops['remove_layer_versions'].get("version")
+
+    try:
+        first = True
+        all_layer_versions = []
+        marker=None
+        while first or marker:
+            first = False
+            layer_versions_retval = lambda_client.list_layer_versions(
+                **remove_none_attributes({
+                    "LayerName": layer_name,
+                    "Marker": marker
+                })
+            )            
+            eh.add_log("Listed Layer Versions", layer_versions_retval)
+
+            marker = layer_versions_retval.get("NextMarker")
+            layer_versions = layer_versions_retval.get("LayerVersions")
+            all_layer_versions.extend(layer_versions)
+
+    except botocore.exceptions.ClientError as e:
+        if e.response['Error']['Code'] in ['ResourceNotFoundException', 'ResourceNotFound']:
+            eh.add_log("Layer Does Not Exist", {"layer_name": layer_name})
+            return None
+        else:
+            eh.retry_error("Listing Layer Versions", 80 if op == "upsert" else 0)
+
+    for layer_version in layer_versions:
+        try:
+            if layer_version.get("Version") != safe_version:
+                print(f"layer_version: {layer_version}")
+                delete_retval = lambda_client.delete_layer_version(LayerName=layer_name, VersionNumber = layer_version.get("Version"))
+                eh.add_log("Deleted Layer Version", layer_version)
+        except Exception as e:
+            print(str(e))
+
+def gen_layer_link(layer_name, region):
+    return f"https://console.aws.amazon.com/lambda/home?region={region}#/layers/{layer_name}"
