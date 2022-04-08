c5cb35d9bab9b756dd22b6bd894240155c174123
Azra Eiden <carter.eiden@gmail.com>
2022-04-08 00:32:49 -0400

    Trying Layer PIP install

--- layer/lambda_function.py
+++ layer/lambda_function.py
@@ -1,26 +1,20 @@
 import boto3
 import botocore
-# import jsonschema
-import json
-import traceback
+from botocore.exceptions import ClientError
 
-# {
-#     "prev_state": prev_state,
-#     "component_def": component_def,
-#     "component_name": component_name,
-#     "op": op,
-#     "s3_object_name": object_name,
-#     "pass_back_data": pass_back_data,
-#     "bucket": bucket,
-#     "repo_id": repo_id,
-#     "project_code": project_code
-# }
+import json
+import os
+import subprocess
+import tempfile
+import traceback
+import zipfile
 
 ALLOWED_RUNTIMES = ["python3.9", "python3.8", "python3.6", "python3.7", "nodejs14.x", "nodejs12.x", "nodejs10.x", "ruby2.7", "ruby2.5"]
 
 
 from extutil import remove_none_attributes, account_context, ExtensionHandler, ext, \
-    current_epoch_time_usec_num, component_safe_name
+    current_epoch_time_usec_num, component_safe_name, create_zip, \
+    handle_common_errors
 
 eh = ExtensionHandler()
 
@@ -46,14 +40,23 @@ def lambda_handler(event, context):
             if prev_state and prev_state.get("props") and (prev_state.get("props").get("name")) != layer_name:
                 eh.add_op("remove_layer_versions", {"name":prev_state.get("props").get("name")})
                 eh.add_op("publish_layer_version")
+                if cdef.get("requirements"):
+                    eh.add_op("add_requirements", cdef.get("requirements"))
+                elif cdef.get("requirements.txt"):
+                    eh.add_op("add_requirements", "$$file")
             elif prev_state:
                 eh.add_op("check_if_update_required")
+                if cdef.get("requirements"):
+                    eh.add_op("add_requirements", cdef.get("requirements"))
+                elif cdef.get("requirements.txt"):
+                    eh.add_op("add_requirements", "$$file")
             else:
                 eh.add_op("publish_layer_version")
 
         elif event.get("op") == "delete":
             eh.add_op("remove_layer_versions", {"name": layer_name})
 
+        add_requirements(bucket, object_name)
         check_if_update_required(prev_state, bucket, object_name)
         publish_layer_version(layer_name, cdef, bucket, object_name, region)
         remove_layer_versions(event.get("op"))
@@ -78,6 +81,57 @@ def get_s3_etag(bucket, object_name):
         eh.add_log("Cound Not Find Zip", {"bucket": bucket, "key": object_name})
         eh.retry_error("Object Not Found")
 
+@ext(handler=eh, op="add_requirements")
+def add_requirements(bucket, object_name):
+    requirements = eh.ops["add_requirements"]
+
+    s3 = boto3.client("s3")
+    with tempfile.TemporaryDirectory() as tmpdir:
+        try:
+            data = s3.get_object(Bucket=bucket, Key=object_name)["Body"]
+            filename = f"{tmpdir}/file.zip"
+            with open(filename, "wb") as f:
+                f.write(data.read())
+        except ClientError as e:
+            handle_common_errors(e, eh, "Download Zipfile Failed", 17)
+        except Exception as e:
+            print(str(e))
+            raise e
+
+        install_directory = f"{tmpdir}/install/"
+        os.mkdir(install_directory)
+        os.chdir(install_directory)
+        with zipfile.ZipFile(filename, 'r') as archive:
+            archive.extractall()
+
+        requirements_file = f"{install_directory}requirements.txt"
+        if requirements != "$$file":
+            with open(requirements_file, "w") as f:
+                f.writelines("%s\n" % i for i in requirements)
+        
+        if os.path.exists(requirements_file):
+            dirs = os.listdir()
+            print(dirs)
+            #We are going to assume there is only one directory, as there should be
+            
+            subprocess.call(f'pip install -r requirements.txt -t ./{dirs[0]}'.split(), stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+            eh.add_log("Requirements installed", {"requirements": requirements})
+        else:
+            eh.add_log("No Requirements to Install", {"files": os.listdir()})
+
+        zipfile_name = f"{tmpdir}/file2.zip"
+        create_zip(zipfile_name, install_directory[:-1])
+
+        try:
+            response = s3.upload_file(zipfile_name, bucket, object_name)
+            eh.add_log("Wrote Requirements to S3", response)
+        except boto3.exceptions.S3UploadFailedError:
+            eh.add_log("Writing Requirements to S3 Failed", {"zipfile_name": zipfile_name, "requirements": requirements})
+            eh.retry_error("S3 Upload Error for Requirements", 25)
+        except ClientError as e:
+            handle_common_errors(e, eh, "Writing Requirements to S3 Failed", 25)
+
+
 @ext(handler=eh, op="check_if_update_required")
 def check_if_update_required(prev_state, bucket, object_name):
     lambda_client = boto3.client("lambda")
@@ -92,7 +146,7 @@ def check_if_update_required(prev_state, bucket, object_name):
             eh.add_log("Got Existing Layer Version", layer_response)
             code_etag = layer_response.get("description")
             print(f"code_etag = {code_etag}")
-        except botocore.exceptions.ClientError as e:
+        except ClientError as e:
             print(str(e))
             if e.response['Error']['Code'] in ["ResourceNotFoundException", "ResourceNotFound"]:
                 eh.add_log("Expected Layer Version Does Not Exist", {'layer_name': props.get("name")})
@@ -153,7 +207,7 @@ def publish_layer_version(layer_name, cdef, bucket, object_name, region):
         if lambda_response['Version'] != 1:
             eh.add_op("remove_layer_versions", {"name": layer_name, "version": lambda_response['Version']})
 
-    except botocore.exceptions.ClientError as e:
+    except ClientError as e:
         print(str(e))
         if e.response['Error']['Code'] in ["InvalidParameterValueException", "CodeStorageExceededException"]:
             eh.add_log("Failure to Publish Layer Version", {'config': desired_config})
@@ -188,7 +242,7 @@ def remove_layer_versions(op):
             layer_versions = layer_versions_retval.get("LayerVersions")
             all_layer_versions.extend(layer_versions)
 
-    except botocore.exceptions.ClientError as e:
+    except ClientError as e:
         if e.response['Error']['Code'] in ['ResourceNotFoundException', 'ResourceNotFound']:
             eh.add_log("Layer Does Not Exist", {"layer_name": layer_name})
             return None
